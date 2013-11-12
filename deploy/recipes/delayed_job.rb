5956483992ad290b2ea389416f1fb98d2fbc0115
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2013-11-12 18:58:16 -0200

    no deploy, as ações que devem acontecer com o delayed_job devem acontecer de acordo com o unicorn

--- deploy/recipes/delayed_job.rb
+++ deploy/recipes/delayed_job.rb
@@ -6,9 +6,9 @@ set_default(:delayed_job_command) { 'bin/delayed_job' }
 # http://migre.me/gyN2k
 set_default(:delayed_job_full_command) { "/bin/su - deploy -c 'cd #{current_path}; RAILS_ENV=#{rails_env} #{delayed_job_command}'" }
 
-after 'deploy:stop',    'delayed_job:stop'
-after 'deploy:start',   'delayed_job:start'
-after 'deploy:restart', 'delayed_job:restart'
+after 'unicorn:stop',    'delayed_job:stop'
+after 'unicorn:start',   'delayed_job:start'
+after 'unicorn:restart', 'delayed_job:restart'
 
 # If you want to use command line options, for example to start multiple
 # workers, define a Capistrano variable delayed_job_args:
