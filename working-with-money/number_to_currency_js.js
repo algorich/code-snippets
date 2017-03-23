e83e09a545b030a496bc9649beeefceca1d856ce
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2017-03-23 18:20:12 -0300

    Update number_to_currency_js.js

--- working-with-money/number_to_currency_js.js
+++ working-with-money/number_to_currency_js.js
@@ -1,30 +1,33 @@
 function numberToCurrency(number) {
-    if (typeof number === 'string')
-        number = parseInt(number.replace(/[^\d]/g, '')) / 100
-    else if (typeof number === 'number' && number % 1 === 0) // integer
-        number = number / 100
-    else if (typeof number === 'number' && number % 1 !== 0) // float
-        number // float is what we want
-    else
-        return
+  var options  = $('#number-to-currency-data').data('options');
 
-    var options  = $('#number-to-currency-data').data('options'),
-        unit      = options.unit,
-        format    = options.format,
-        separator = options.separator,
-        delimiter = options.delimiter;
+  if (!options) throw('You should define a element with id="number-to-currency-data" and data attributes');
 
-    if (number < 0) number = Math.abs(number);
+  if (typeof number === 'string')
+    number = parseInt(number.replace(/[^\d]/g, '')) / 100.0
+  else if (typeof number === 'number' && number % 1 === 0) // integer
+    number = number / 100.0
+  else if (typeof number === 'number' && number % 1 !== 0) // float
+    number // float is what we want
+  else
+    throw("Not recognized number");
 
-    if (isNaN(number)) number = '0';
+  var unit      = options.unit,
+      format    = options.format,
+      separator = options.separator,
+      delimiter = options.delimiter;
 
-    var cents = Math.floor((number * 100 + 0.5)%100);
-    if (cents < 10) cents = '0' + cents;
+  if (number < 0) number = Math.abs(number);
 
-    number = Math.floor((number * 100 + 0.5)/100).toString();
+  if (isNaN(number)) number = '0';
 
-    for (var i = 0; i < Math.floor((number.length-(1 + i))/3); i++)
-        number = number.substring(0, number.length-(4 * i + 3)) + delimiter + number.substring(number.length-(4 * i + 3));
+  var cents = Math.floor((number * 100 + 0.5) % 100);
+  if (cents < 10) cents = '0' + cents;
 
-    return format.replace('%u', unit).replace('%n', number + separator + cents);
+  number = Math.floor((number * 100 + 0.5) / 100).toString();
+
+  for (var i = 0; i < Math.floor((number.length-(1 + i)) / 3); i++)
+    number = number.substring(0, number.length - (4 * i + 3)) + delimiter + number.substring(number.length - (4 * i + 3));
+
+  return format.replace('%u', unit).replace('%n', number + separator + cents);
 }
