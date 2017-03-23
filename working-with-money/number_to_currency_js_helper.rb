4e69dd31caf893291a6480913f98bfd2bb191988
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2017-03-23 18:19:21 -0300

    Update number_to_currency_js_helper.rb

--- working-with-money/number_to_currency_js_helper.rb
+++ working-with-money/number_to_currency_js_helper.rb
@@ -1,6 +1,6 @@
 module NumberToCurrencyJsHelper
   def number_to_currency_js(options={})
-    options = ActiveSupport::NumberHelper.send(:i18n_format_options, options[:locale], :currency)
+    options = I18n.translate(:"number.currency.format", locale: options[:locale], default: {})
     "<span style='display: none;' id='number-to-currency-data' data-options='#{options.to_json}'></span>".html_safe
   end
 end
